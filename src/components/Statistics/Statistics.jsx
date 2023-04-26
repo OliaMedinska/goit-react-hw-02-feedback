b21a5c8c33f224bd6bab576b55233302ac2fe140
OliaMedinska <medinska2@gmail.com>
2023-04-26 14:34:51 +0300

    new style

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,23 +2,23 @@ import PropTypes from 'prop-types';
 import './Statistics.css';
 
 const Statistics = ({ good, neutral, bad, total, positiveFeedback }) => {
-    return (
-        <ul>
-            <li>Good: {good}</li>
-            <li>Neutral: {neutral}</li>
-            <li>Bad: {bad}</li>
-            <li>Total: {total}</li>
-            <li>Positive feedback: {positiveFeedback}</li>
-        </ul>
-    )
+  return (
+    <ul>
+      <li className="statistics-item">Good: {good}</li>
+      <li className="statistics-item">Neutral: {neutral}</li>
+      <li className="statistics-item">Bad: {bad}</li>
+      <li className="statistics-item">Total: {total}</li>
+      <li className="statistics-item">Positive feedback: {positiveFeedback}</li>
+    </ul>
+  );
 };
 
 Statistics.propTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positiveFeedback: PropTypes.string.isRequired
-}
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positiveFeedback: PropTypes.string.isRequired,
+};
 
 export default Statistics;
